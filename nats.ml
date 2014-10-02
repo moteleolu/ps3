e21d84ac8514aa45ff3084d623b1c3ee280a2a46
Kayla <krm226@cornell.edu>
2014-10-01 20:04:57 -0400

    p3e4

--- nats.ml
+++ nats.ml
@@ -9,12 +9,13 @@ module type NATN = sig
   val zero : t
 
   (*val one is the identity element for multiplication. Multiplying a number n 
-    by val one will retrun n.
+    by val one will return n.
   *)
   val one : t
 
-  (*Precondition: Takes two valid natural numbers as arguments.
-    Postcondition: Returns the sum of the two values as a natural number.
+  (*Precondition: Takes two arguments of the same type.
+    Postcondition: Returns the sum of the two values as a value of the same 
+    type of the arguments.
     Computes the sum of two values. 
     + is associative: (a+b)+c = a+(b+c)
     + is commutative: a+b = b+a
@@ -22,8 +23,9 @@ module type NATN = sig
   *)
   val ( + ) : t -> t -> t
 
-  (*Precondition: Takes two valid natural numbers as arguments.
-    Postcondition: Returns the product of the two values as a natural number.
+  (*Precondition: Takes two arguments of the same type.
+    Postcondition: Returns the product of the two values as a value of the 
+    same type of the arguments.
     Computes the product of two values.
     * is associative: (a*b)*c = a*(b*c)
     * is commutative: a*b = b*a
@@ -32,7 +34,7 @@ module type NATN = sig
   *)
   val ( * ) : t -> t -> t 
 
-  (*Precondition: Takes two valid natural numbers as arguments.
+  (*Precondition: Takes two arguments of the same type.
     Postcondition: Returns true if the first argument is less than the second 
     argument and false otherwise.
     Determines whether the first argument is less than the second argument, 
@@ -40,7 +42,7 @@ module type NATN = sig
   *)
   val ( < ) : t -> t -> bool
 
-  (*Precondition: Takes two valid natural numbers as arguments.
+  (*Precondition: Takes two arguments of the same type.
     Postcondition: Returns true if the two arguments are equal to each other 
     and false otherwise.
     Determines whether the first argument is equal to the second argument, 
@@ -50,7 +52,7 @@ module type NATN = sig
 			    
   exception Unrepresentable
 
-  (*Precondition: Takes a valid natural number as an argument.
+  (*Precondition: Takes one valid natural number as an argument.
     Postcondition: Returns a valid value of type int.
     Converts a natural number to an int if the natural number can be 
     represented as a valid int. If it cannot, an exception is thrown.
@@ -58,7 +60,7 @@ module type NATN = sig
   val int_of_nat: t -> int
 
   (*Precondition: Takes a valid type int as an argument.
-    Postcondition: Returns a valide natrual number.
+    Postcondition: Returns the int as a valid natrual number.
     Converts a value of type int to a natural number if the int can be 
     represented as a valid natrual number. If it cannot, an exception is thrown.
   *)
@@ -85,27 +87,52 @@ let sum_overflows (i1:int) (i2:int) : bool =
    types specified in the problem set. *)
 
 module IntNat: NATN = struct 
-type t = int 
-exception Unrepresentable
-let zero= 0
-let one =1
+  type t = int 
+  exception Unrepresentable
+  let zero= 0
+  let one =1
 
 
-type sign = Positive | Negative 
+  type sign = Positive | Negative 
 
-let sign_int (n:int) : sign = if n >= 0 then Positive else Negative
+  let sign_int (n:int) : sign = if n >= 0 then Positive else Negative
 
-let sum_overflows (i1:int) (i2:int) : bool = 
-  sign_int i1 = sign_int i2 && sign_int(i1 + i2) <> sign_int i1
+  let sum_overflows (i1:int) (i2:int) : bool = 
+    sign_int i1 = sign_int i2 && sign_int(i1 + i2) <> sign_int i1
 
-let prod_overflows (i1:int) (i2:int) :bool=
-  sign_int i1 = sign_int i2 && sign_int (i1 * i2)<> sign_int i1
+  let prod_overflows (i1:int) (i2:int) :bool=
+    sign_int i1 = sign_int i2 && sign_int (i1 * i2)<> sign_int i1
 
-let ( + ) t1 t2 = if t1 + t2 < 0 || sum_overflows t1 t2 then raise (Unrepresentable) else t1+t2  
-let ( * ) t1 t2 = if t1 * t2 < 0 || prod_overflows t1 t2 then then raise (Unrepresentable) else t1*t2
-let ( < ) t1 t2 =  t1>t2
-let ( === ) t1 t2 = t1=t2
+  let ( + ) t1 t2 = if t1 + t2 < 0 || sum_overflows t1 t2 then raise (Unrepresentable) else t1+t2  
+  let ( * ) t1 t2 = if t1 * t2 < 0 || prod_overflows t1 t2 then raise (Unrepresentable) else t1*t2
+  let ( < ) t1 t2 =  t1>t2
+  let ( === ) t1 t2 = t1=t2
 
-let int_of_nat t1 = t1
-let nat_of_int n1 = n1
+  let int_of_nat t1 = t1
+  let nat_of_int n1 = n1
 end
+
+
+module NatConvertFn (N: NATN) = struct
+  let int_of_nat (n : N.t) : int =
+    (*match n with 
+      zero -> 0
+    | one -> 1
+    | (one + nat) -> int_of_nat nat + 1*)
+    (*let count = N.zero in 
+    let ans = 0 in 
+    while ((not (count N.=== n)) & count <= max_int) do 
+      let ans = ans + 1 in 
+      let count = count + N.one in count
+    done*)
+    N.int_of_nat (n)
+
+    (*if max_int < n.int_of_nat then raise (Unrepresentable) else n.int_of_nat*)
+  let rec nat_of_int (n: int) : N.t = 
+    (*match n with 
+      0 -> N.zero
+    | 1 -> N.one
+    | _ -> (+) N.one nat_of_int(n-1)*)
+    N.nat_of_int (n)
+end
+
